82e4c570311b01579c7a6c04f2d748485695d3a1
Mantas Misiūnas <247150254+mantasmisiun@users.noreply.github.com>
2026-09-01 23:00:35 +0300

    Added tflint to workflows (#5)
    * Added tflint to workflows
    
    * fixed tflint flagged issues

--- modules/docker-service/versions.tf
+++ modules/docker-service/versions.tf
@@ -1,7 +1,9 @@
 terraform {
+  required_version = ">= 1.5"
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
+      version = "4.5.0"
     }
   }
 }
